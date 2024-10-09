ea0c923c55380254f92a2adcbc5fee378d1ac823
boshna1 <joshua.dinata@ontariotechu.net>
2024-10-09 15:44:19 -0400

    added lut  and colorgrading

--- Assets/Scritps/Camera.cs
+++ Assets/Scritps/Camera.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
-
+    public Material m_renderMaterial;
+    public Material lutMaterial;
 
     // Update is called once per frame
     void Update()
     {
         transform.position += new Vector3(0, 0, -0.1f);
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Debug.Log("OnRenderImage called"); // Check if this message appears
+        if (lutMaterial != null)
+        {
+            Graphics.Blit(source, destination, lutMaterial);
+        }
+        else
+        {
+            Graphics.Blit(source, destination);
+        }
     }
 }
